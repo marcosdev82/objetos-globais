9e2b737e5f01187f87bfa7ba0cfca84fcb0000d9
mikek182 <marcosvinicius@geekwork.com.br>
2025-05-14 13:59:50 -0300

    eval

---
+++ eval.js
@@ -0,0 +1,5 @@
+// A função eval() computa um código JavaScript representado como uma string
+
+console.log(eval(new String("2 + 2"))); // retorna um objeto String contendo "2 + 2"
+console.log(eval("2 + 2")); // retorna 4
+
